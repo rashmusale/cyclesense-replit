349e65663c82b0adf6114b08129ad69165b78aa0
rashmusale <rash.musale@gmail.com>
2025-11-06 18:44:35 +0530

    Update main.tsx
    set basename
    add reactdom & browserrouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/cyclesense-replit">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
